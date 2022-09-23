dfbe24e690c6cf3bc60a86fce3f9b295c1af23dc
RobinCMier <robintmier@gmail.com>
2022-09-23 16:34:51 +0200

    trying to add img to json and display. Getting stuck on the path, but the title does show

---
+++ src/components/ArtPanel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>this is artpanel</h1>
+  </div>
+</template>
+<script>
+// import data and tools
+export default {
+  name: "Art-Panel",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <template>
   <main>
     <h1>Gallery:</h1>
-    <p>This is where gallery should go?</p>
-    <div>{{ artworks }}</div>
+    <div v-for="artwork in artworks" :key="artwork.id">
+      <h1>{{ artwork.name }}</h1>
+      <img :src="artwork.img" />
+      <img src="@/mock/images/portfolio1.jpg" />
+      <!-- path is not found. artwork name is found so it's sth to do w the img path-->
+    </div>
   </main>
 </template>
 
 <script>
 // import data and tools
 import artworks from "../mock/artworks.json";
+import ArtPanel from "../components/ArtPanel.vue";
 // import components
 
 export default {
@@ -18,5 +23,6 @@ export default {
       artworks: artworks,
     };
   },
+  components: { ArtPanel },
 };
 </script>
