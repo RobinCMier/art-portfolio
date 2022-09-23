73c688294f13e85e8653d7710301970687e68860
RobinCMier <robintmier@gmail.com>
2022-09-23 15:39:12 +0200

    set up json file with mock data, calling it in homeview

---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>this will be the picture</h1>
+  </section>
+  <section>
+    <h1>This will be the banner</h1>
+  </section>
+</template>
+
+<script>
+export default {
+ ]
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Gallery:</h1>
+    <p>This is where gallery should go</p>
+    <div>{{ artworks }}</div>
   </main>
 </template>
+
+<script>
+import artworks from "../mock/artworks.json";
+
+export default {
+  name: "Home-view",
+
+  data() {
+    return {
+      artworks: artworks,
+    };
+  },
+};
+</script>
